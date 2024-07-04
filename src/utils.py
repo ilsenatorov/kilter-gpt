22a833dee5ee682e87718313fbe8481b1a80823f
Ilya Senatorov <il.senatorov@protonmail.com>
2024-07-04 17:01:30 +0200

    updated plotter

--- src/utils.py
+++ src/utils.py
@@ -39,8 +39,7 @@ class Tokenizer:
         return res
 
     def encode(self, frames: str) -> torch.Tensor:
-        """Assumes whitespace separated"""
-        split = ["[BOS]"] + frames.split() + ["[EOS]"]
+        split = ["[BOS]"] + self.split_tokens(frames) + ["[EOS]"]
         return torch.tensor([self.encode_map[x] for x in split], dtype=torch.long)
 
     def encode_batch(self, frames: list[str]) -> list[torch.Tensor]:
@@ -53,81 +52,76 @@ class Tokenizer:
                 decoded.append(self.decode_map[token])
             else:
                 decoded.append("[UNK]")
-        return " ".join(decoded)
+        return "".join(decoded)
 
     def decode_batch(self, x: Iterable[torch.Tensor]) -> list[str]:
         return [self.decode(y) for y in x]
 
     def _get_token_map(self) -> dict[str, int]:
-        """Assumes whitespace separated frames."""
         tokens = ["[BOS]", "[EOS]", "[MASK]", "[UNK]", "[PAD]", "r12", "r13", "r14", "r15"]
         for frame in self.data:
-            for token in frame.split():
+            for token in self.split_tokens(frame):
                 if token not in tokens:
                     tokens.append(token)
         encode_map = {token: idx for idx, token in enumerate(tokens)}
         return encode_map
 
 
-class EncoderDecoder:
-    """Converts frames to tensors and back.
-    If given tensor - returns string and angle.
-    If given string and angle - returns (5,48,48) tensor.
-    """
+class Plotter:
 
     def __init__(self):
-        holds = pd.read_csv("data/raw/holds.csv", index_col=0)
+        # holds = pd.read_csv("data/raw/holds.csv", index_col=0)
         image_coords = pd.read_csv("figs/image_coords.csv", index_col=0)
-        self.coord_to_id = self._create_coord_to_id(holds)
-        self.id_to_coord = self._create_id_to_coord(holds)
+        # self.coord_to_id = self._create_coord_to_id(holds)
+        # self.id_to_coord = self._create_id_to_coord(holds)
         self.image_coords = self._create_image_coords(image_coords)
 
-    def _create_coord_to_id(self, holds: pd.DataFrame):
-        hold_lookup_matrix = np.zeros((48, 48), dtype=int)
-        for i in range(48):
-            for j in range(48):
-                hold = holds[(holds["x"] == (i * 4 + 4)) & (holds["y"] == (j * 4 + 4))]
-                if not hold.empty:
-                    hold_lookup_matrix[i, j] = int(hold.index[0])
-        return hold_lookup_matrix
+    # def _create_coord_to_id(self, holds: pd.DataFrame):
+    #     hold_lookup_matrix = np.zeros((48, 48), dtype=int)
+    #     for i in range(48):
+    #         for j in range(48):
+    #             hold = holds[(holds["x"] == (i * 4 + 4)) & (holds["y"] == (j * 4 + 4))]
+    #             if not hold.empty:
+    #                 hold_lookup_matrix[i, j] = int(hold.index[0])
+    #     return hold_lookup_matrix
 
-    def _create_id_to_coord(self, holds):
-        id_to_coord = holds[["x", "y"]]
-        id_to_coord = (id_to_coord - 4) // 4
-        return id_to_coord.transpose().to_dict(orient="list")
+    # def _create_id_to_coord(self, holds):
+    #     id_to_coord = holds[["x", "y"]]
+    #     id_to_coord = (id_to_coord - 4) // 4
+    #     return id_to_coord.transpose().to_dict(orient="list")
 
     def _create_image_coords(self, image_coords: pd.DataFrame):
         return {name: (row["x"], row["y"]) for name, row in image_coords.iterrows()}
 
-    def str_to_tensor(self, frames: str, angle: float) -> torch.Tensor:
-        angle_matrix = torch.ones((1, 48, 48), dtype=torch.float32) * (angle / 70)
-        matrix = torch.zeros((4, 48, 48), dtype=torch.float32)
-        for frame in frames.split("p")[1:]:
-            hold_id, color = frame.split("r")
-            hold_id, color = int(hold_id), int(color) - 12
-            coords = self.id_to_coord[hold_id]
-            matrix[color, coords[0], coords[1]] = 1
-        return torch.cat((matrix, angle_matrix), dim=0)
+    # def str_to_tensor(self, frames: str, angle: float) -> torch.Tensor:
+    #     angle_matrix = torch.ones((1, 48, 48), dtype=torch.float32) * (angle / 70)
+    #     matrix = torch.zeros((4, 48, 48), dtype=torch.float32)
+    #     for frame in frames.split("p")[1:]:
+    #         hold_id, color = frame.split("r")
+    #         hold_id, color = int(hold_id), int(color) - 12
+    #         coords = self.id_to_coord[hold_id]
+    #         matrix[color, coords[0], coords[1]] = 1
+    #     return torch.cat((matrix, angle_matrix), dim=0)
 
-    def tensor_to_str(self, matrix: torch.Tensor) -> str:
-        angle = ((matrix[-1].mean() * 70 / 5).round() * 5).long().item()
-        matrix = matrix[:-1, :, :].round().long()
-        frames = []
-        counter = [0, 0, 0, 0]
-        for color, x, y in zip(*torch.where(matrix)):
-            counter[color] += 1
-            color, x, y = color.item(), x.item(), y.item()
-            # too many start/end holds
-            if counter[color] > 2 and color in [0, 2]:
-                continue
-            hold_id = self.coord_to_id[x, y]
-            # wrong hold position
-            if hold_id == 0:
-                continue
-            role = color + 12
-            frames.append((hold_id, role))
-        sorted_frames = sorted(frames, key=lambda x: x[0])
-        return ("".join([f"p{hold_id}r{role}" for hold_id, role in sorted_frames]), angle)
+    # def tensor_to_str(self, matrix: torch.Tensor) -> str:
+    #     angle = ((matrix[-1].mean() * 70 / 5).round() * 5).long().item()
+    #     matrix = matrix[:-1, :, :].round().long()
+    #     frames = []
+    #     counter = [0, 0, 0, 0]
+    #     for color, x, y in zip(*torch.where(matrix)):
+    #         counter[color] += 1
+    #         color, x, y = color.item(), x.item(), y.item()
+    #         # too many start/end holds
+    #         if counter[color] > 2 and color in [0, 2]:
+    #             continue
+    #         hold_id = self.coord_to_id[x, y]
+    #         # wrong hold position
+    #         if hold_id == 0:
+    #             continue
+    #         role = color + 12
+    #         frames.append((hold_id, role))
+    #     sorted_frames = sorted(frames, key=lambda x: x[0])
+    #     return ("".join([f"p{hold_id}r{role}" for hold_id, role in sorted_frames]), angle)
 
     def plot_climb(self, frames: str):
         assert isinstance(frames, str), f"Input must be frames! Got {type(frames)}"
@@ -152,14 +146,6 @@ class EncoderDecoder:
         except Exception as e:
             pass
         return image
-
-    def __call__(self, *args):
-        if len(args) == 1:
-            return self.tensor_to_str(*args)
-        elif len(args) == 2:
-            return self.str_to_tensor(*args)
-        else:
-            raise ValueError(f"Only 2 input args allowed! You provided {len(args)}")
 
 
 def pad_to(
--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import torch
 from lightning.pytorch import Trainer
 from lightning.pytorch.loggers import WandbLogger
@@ -9,39 +11,36 @@ from src.models.gpt import GPTModel
 torch.set_float32_matmul_precision("medium")
 
 
+@dataclass
 class Config:
-    batch_size = 2048
+    batch_size = 4096
     epochs = 1000
-    vocab_size = 536
+    vocab_size = 550
     lr = 6e-4
     wd = 1e-5
     n_embed = 256
     num_blocks = 4
     num_heads = 4
     head_size = n_embed // num_heads
-    context_len = 32
+    context_len = 64
     attn_drop_value = 0.2
     multihead_drop_value = 0.2
     ffn_drop_value = 0.2
+    min_tokens = 5
 
-
-config_dict = dict(vars(Config))
-del [config_dict["__module__"]]
-del [config_dict["__dict__"]]
-del [config_dict["__weakref__"]]
-del [config_dict["__doc__"]]
 
 config = Config()
 
-ds = KilterGPTDataset("data/raw/gpt_subset.csv", context_len=config.context_len)
+ds = KilterGPTDataset(
+    "data/raw/all_climbs.csv", context_len=config.context_len, min_tokens=config.min_tokens, deduplicate=True
+)
 dl = DataLoader(ds, batch_size=config.batch_size, shuffle=True, pin_memory=True, num_workers=8)
 model = GPTModel(config)
 
 trainer = Trainer(
     devices=-1,
     max_epochs=config.epochs,
-    logger=[WandbLogger(project="kilter-gpt")],
-    # detect_anomaly=True,
+    logger=[WandbLogger(project="kilter-gpt", config=vars(config))],
 )
 
 trainer.fit(model, dl)
