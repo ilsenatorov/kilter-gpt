e9a4cb657943d02a0a65452503153e6c989637f9
Ilya Senatorov <il.senatorov@protonmail.com>
2024-07-04 22:10:43 +0200

    forgot the data files

---
+++ src/data/datasets.py
@@ -0,0 +1,41 @@
+import random
+
+import pandas as pd
+from torch.utils.data import Dataset
+
+from ..utils import Tokenizer, pad_to, shuffle_holds
+
+
+class KilterGPTDataset(Dataset):
+    def __init__(
+        self,
+        filename,
+        context_len: int = 32,  # 1 hold == 2 tokens
+        min_tokens: int = 5,  # smallest number of tokens in a sequence
+        deduplicate: bool = True,
+    ):
+        self.df = pd.read_csv(filename)
+        if deduplicate:
+            self.df = self.df.drop_duplicates(subset=["frames"])
+        self.tokenizer = self._get_tokenizer()
+        self.context_len = context_len
+        self.min_tokens = min_tokens
+        self.pad_token = self.tokenizer.encode_map["[PAD]"]
+        self.eos_token = self.tokenizer.encode_map["[EOS]"]
+        self.bos_token = self.tokenizer.encode_map["[BOS]"]
+
+    def _get_tokenizer(self):
+        return Tokenizer(self.df["frames"])
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        """Get a random contiguous sequence of tokens from the frames column. Pad left to context_len."""
+        t = self.tokenizer.encode(shuffle_holds(self.df.iloc[idx]["frames"]))
+        end = random.randint(self.min_tokens, t.size(0))
+        start = max(0, end - self.context_len - 1)  # buffer start
+        buffer = t[start:end]
+        x = buffer[:-1]
+        y = buffer[1:]
+        return pad_to(x, self.context_len, self.pad_token), pad_to(y, self.context_len, self.pad_token)
