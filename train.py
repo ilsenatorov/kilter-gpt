be0e35a702661a75ae926390c22f3b908cd56aef
Ilya Senatorov <il.senatorov@protonmail.com>
2024-07-05 15:29:25 +0200

    consistency fix
    Former-commit-id: bf14069aa8e0aa45a379ea469ab36d9831810ce0

--- train.py
+++ train.py
@@ -1,15 +1,13 @@
 from dataclasses import dataclass
 
-import torch
 import lightning as L
+import torch
 from lightning.pytorch import Trainer
 from lightning.pytorch.loggers import WandbLogger
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
 from src.data.datasets import KilterGPTDataset
 from src.models.gpt import GPTModel
-from torch.utils.data import random_split
-
 
 L.seed_everything(42)
 torch.set_float32_matmul_precision("high")
@@ -22,15 +20,15 @@ class Config:
     vocab_size = 600
     lr = 1e-3
     wd = 1e-5
-    n_embed = 128
-    num_blocks = 4
-    num_heads = 4
+    n_embed = 512
+    num_blocks = 8
+    num_heads = 8
     head_size = n_embed // num_heads
     context_len = 64
     attn_drop_value = 0.2
     multihead_drop_value = 0.2
     ffn_drop_value = 0.2
-    min_tokens = 10
+    min_tokens = 20
     angle = True
     grade = True
 
@@ -41,7 +39,7 @@ ds = KilterGPTDataset(
     "data/raw/climbs.csv",
     context_len=config.context_len,
     min_tokens=config.min_tokens,
-    deduplicate=True,
+    deduplicate=False,
     angle=config.angle,
     grade=config.grade,
 )
@@ -55,11 +53,11 @@ model = GPTModel(config)
 trainer = Trainer(
     devices=-1,
     max_epochs=config.epochs,
-    logger=[WandbLogger(project="kilter-gpt", config=vars(config), log_model="all")],
+    logger=[WandbLogger(project="kilter-gpt", config=vars(config), log_model=True)],
     precision="bf16-mixed",
     callbacks=[
-        L.pytorch.callbacks.EarlyStopping(monitor="val_loss", patience=30),
-        L.pytorch.callbacks.ModelCheckpoint(monitor="val_loss", mode="min"),
+        L.pytorch.callbacks.EarlyStopping(monitor="val/loss", patience=30),
+        L.pytorch.callbacks.ModelCheckpoint(monitor="val/loss", mode="min"),
     ],
 )
 
