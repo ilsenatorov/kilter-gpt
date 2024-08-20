dfa815ceea053de2fadb0d98c161563c422a3eeb
Ilya Senatorov <il.senatorov@protonmail.com>
2024-08-20 11:59:40 +0200

    Revert "added scheduler tests"
    This reverts commit 96a9f1aa264658c4bdc9f751e60a179f021894dd.

--- kiltergpt/utils.py
+++ kiltergpt/utils.py
@@ -67,9 +67,7 @@ def str_to_bool(value: str) -> bool:
 
 
 class WarmupCosineSchedule(torch.optim.lr_scheduler.LambdaLR):
-    """Linear warmup and then cosine decay. Coeff is the original lr multiplier.
-    Goes from lr * start_coeff up to lr in warmup_steps, then decays to lr * end_coeff over total_steps.
-    After step total_steps, stays at lr * end_coeff."""
+    """Linear warmup and then cosine decay. Coeff is the original lr multiplier."""
 
     def __init__(
         self,
