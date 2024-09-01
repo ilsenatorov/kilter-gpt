28321aa93e803137cf1afd59e8309891299f8656
epivoca <artfom02@gmail.com>
2024-09-01 12:29:38 +0300

    added init file and TODOs for the future

--- src/app/models/generation.py
+++ src/app/models/generation.py
@@ -12,5 +12,6 @@ class GenerationParams(BaseModel):
 
 class Generation(BaseModel):
     params: GenerationParams
-    climb: str
+    climb: str  # TODO: think about extracting it to an independent model in case if we will decide to make it all in one service for all kilter climbs?
+                # it will mean, that current solution to store all data in the single table is not the most genious idea.
     feedback: Optional[str]
