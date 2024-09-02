3e5be6208262590204b2b0c4c911b098a9be8eaa
epivoca <artfom02@gmail.com>
2024-09-02 23:01:24 +0300

    added web server settings

--- src/app/config.py
+++ src/app/config.py
@@ -8,6 +8,11 @@ ENV_FILE = os.environ.get("ENV_FILE") or ".env"
 
 
 class Settings(BaseSettings):
+    BIND_IP: str = "0.0.0.0"
+    BIND_PORT: int = 8000
+    HOSTNAME: str = "default"
+    BACKEND_CORS_ORIGINS: str = "*"
+
     # ==== Logging settings ====
     LOGGING_LEVEL: str = "DEBUG"
     # TODO: provide "LOGGING_PATH" with logging config
