aeaf1e9d006402ea20ef5485a840e3d0b6dd8d12
epivoca <artfom02@gmail.com>
2024-09-02 21:16:18 +0300

    cleaned up db setup due to migration to supabase

--- src/app/db/__init__.py
+++ src/app/db/__init__.py
@@ -1,4 +1,7 @@
 """Place here all db models."""
+from sqlalchemy.orm import configure_mappers
 
 from .engine import engine
 from .generations import GenerationsData
+
+configure_mappers()
--- src/app/db/engine.py
+++ src/app/db/engine.py
@@ -2,31 +2,6 @@
 Setup DB connection.
 """
 
-from sqlalchemy import create_engine
-from sqlalchemy.engine import Engine
 from src.app.config import settings
 
-
-def get_pg_engine(
-    database_host: str = "127.0.0.1",
-    database_port: str = "5429",
-    database_user: str = "postgres",
-    database_password: str = "postgres",
-    database_name: str = "postgres",
-) -> Engine:
-    db_string = f"postgresql://{database_user}:{database_password}@{database_host}:{database_port}/{database_name}"
-
-    return create_engine(db_string)
-
-
-def create_db_engine() -> Engine:
-    return get_pg_engine(
-        database_host=settings.DB_URL,
-        database_port=settings.DB_PORT,
-        database_name=settings.DB_NAME,
-        database_user=settings.DB_USER,
-        database_password=settings.DB_PASSWORD,
-    )
-
-
-engine = create_db_engine()
+# TODO: setup supabase client here
