253df0347e765cdcebf46fc1553e050aab9f0f12
epivoca <artfom02@gmail.com>
2024-09-21 17:51:33 +0300

    Udated requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="kiltergpt",
+    name="src",
     version="0.1.0",
     description="A project for training models to generate Kilterboard climbs.",
     author="Ilya Senatorov",
