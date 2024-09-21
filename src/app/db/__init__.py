2f992ff8d9068dce760d5564446f1efca9900e11
epivoca <artfom02@gmail.com>
2024-09-21 17:11:37 +0300

    Updated db layers & data models

--- src/app/db/__init__.py
+++ src/app/db/__init__.py
@@ -1,4 +1,4 @@
 """Place here all db models."""
 
 from .client import supabase_client
-from .kilter import KilterClimbs, KilterClimbStats, GenerationMetadata, GeneratedClimbFeedback
+from .kilter import KilterClimbStats, GenerationMetadata, GeneratedClimbFeedback
--- src/app/db/client.py
+++ src/app/db/client.py
@@ -5,5 +5,4 @@ Setup DB connection.
 from src.app.config import settings
 from supabase import create_client
 
-
 supabase_client = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
